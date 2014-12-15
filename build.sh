8ba9ab0234a295727e0ba2211d770ce5ab8aac1f
Keith Suderman <suderman@anc.org>
2014-12-15 15:21:50 -0500

    Working on Makefile

--- build.sh
+++ build.sh
@@ -1,7 +1,44 @@
 #!/bin/bash
 
-for schema in `ls *.schema` ; do
-	json=`echo $schema | sed "s/\./-/"`
-	json=$json.json
-	echo $json
+function schemata()
+{
+	for schema in `ls *.schema` ; do
+		json=`echo $schema | sed "s/\./-/"`
+		json=$json.json
+		jsonc -p -d 4 -i $schema -o $json 
+		echo "Compiled $schema --> $json"
+	done
+}
+
+function examples()
+{
+	for text in ` ls *.txt`; do
+		json=`echo $text | sed "s/\.txt$/.json/"`
+		jsonc -p -i $text -o $json 
+		echo "Compiled $text --> $json"
+	done
+}
+
+while [ -n "$1" ] ; do
+	case $1 in
+		schemata)
+			schemata
+			;;
+		examples)
+			examples
+			;;
+		all)
+			schemata
+			examples
+			;;
+		*)
+			echo "Invalid option $1"
+			exit 1
+			;;
+	esac
+	shift
 done
+echo "Done."
+
+
+
