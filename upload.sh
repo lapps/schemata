e9aa5cf73077997d50b92522c672f93b2cfee839
Keith Suderman <suderman@cs.vassar.edu>
2018-02-02 20:28:32 -0500

    Minor cleanup

--- upload.sh
+++ upload.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+#
+# This script assumes that $USER has an account on anc.org with their
+# ssh key installed so that passwordless SSH connections work.
+#
 if [ -z "$1" ] ; then
 	echo "No version number specified."
 	exit 1
@@ -10,9 +14,9 @@ DEST=/home/www/anc/LAPPS/vocab/schema/$V
 echo "Uploading to $DEST"
 
 for schema in `ls schemata/*.json` ; do
-	anc-put $schema $DEST
+	scp -P 22022 $schema $USER@anc.org:$DEST
 done
 for schema in `ls *.schema` ; do
-	anc-put $schema $DEST
+	scp -P 22022 $schema $USER@anc.org:$DEST
 done
 
